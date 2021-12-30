dc59dbf896c792f6161b9ed21084a4cbaf3678b5
goodboy3 <zhangzhengbo@hotmail.com>
2021-12-30 10:49:57 +0800

    commit

--- logInterface.go
+++ logInterface.go
@@ -30,30 +30,14 @@ const (
 type (
 	// Logger defines the logging interface.
 	Logger interface {
-		Output() io.Writer
 		SetOutput(w io.Writer)
-		GetLevel() LogLevel
 		SetLevel(v LogLevel)
 		Trace(i ...interface{})
-		//Traceln(i ...interface{})
-		//Tracef(format string, args ...interface{})
 		Debug(i ...interface{})
-		//Debugln(i ...interface{})
-		//Debugf(format string, args ...interface{})
 		Info(i ...interface{})
-		//Infoln(i ...interface{})
-		//Infof(format string, args ...interface{})
 		Warn(i ...interface{})
-		//Warnln(i ...interface{})
-		//Warnf(format string, args ...interface{})
 		Error(i ...interface{})
-		//Errorln(i ...interface{})
-		//Errorf(format string, args ...interface{})
 		Fatal(i ...interface{})
-		//Fatalln(i ...interface{})
-		//Fatalf(format string, args ...interface{})
 		Panic(i ...interface{})
-		//Panicln(i ...interface{})
-		//Panicf(format string, args ...interface{})
 	}
 )
